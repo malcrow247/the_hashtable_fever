beaf0ec376d02bd669d0467453451eb3fff149f6
malcrow <melehassyti@gmail.com>
2026-03-12 19:27:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,67 +1,30 @@
-#include "list.h"
-#include "vector.h"
 #include "hashtable.h"
 
-int main()
-{
+int main() {
     hashtable h;
+
     int status;
 
     status = hashtable_create(&h);
-    if (status != OK)
-    {
-        printf("Не получилось создать хеш таблицу\n");
-        return ERR;
+
+    if (status != 0) {
+        printf("Ошибка во время создания хеш таблицы\n");
+        return 0;
     }
 
-    for (int i = 0; i < 10; ++i)
-    {
-        status = hashtable_set_value(&h, i, i * i);
-        if (status != OK)
-        {
-            printf("Не получилось вставить элемент %d\n", i);
+    for (int i = 1; i < 11; ++i) {
+        hashtable_set(&h, i, i * i);
+    }
 
-            hashtable_free(&h);
+    for (int i = 1; i < 11; ++i) {
+        int result = -1;
+        status = hashtable_search(&h, i, &result);
 
-            return ERR;
+        printf("поиск по %d, статус %d\n", i, status);
+        if (status == 0) {
+            printf("%d\n", result);
         }
     }
-
-    int result;
-    for (int i = 0; i < 10; ++i)
-    {
-        status = hashtable_get_value(&h, i, &result);
-        if (status != OK && status != NOTEXISTS)
-        {
-            printf("Не получилось вытащить элемент %d\n", i);
-            hashtable_free(&h);
-            return ERR;
-        }
-        else if (status == NOTEXISTS)
-        {
-            printf("Не нашлось элемента по индексу %d\n", i);
-        }
-        else
-        {
-            printf("%d : %d\n", i, result);
-        }
-    }
-
-    int i = 1;
-    status = hashtable_delete(&h, i);
-
-    if (status != OK && status != NOTEXISTS)
-    {
-        printf("Ошибка удаления\n");
-        return ERR;
-    }
-    else if (status == NOTEXISTS)
-    {
-        printf("Не найден элемент %d для удаления\n", i);
-        return ERR;
-    }
-    else
-        printf("Элемент %d удален\n", i);
 
     hashtable_free(&h);
 
