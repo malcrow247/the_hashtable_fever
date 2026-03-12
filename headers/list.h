3215969ba97d0f5a730a5ed813bcfbad5f4dfb81
malcrow <melehassyti@gmail.com>
2026-03-12 18:49:46 +0300

    list.h

--- headers/list.h
+++ headers/list.h
@@ -3,25 +3,23 @@
 
 #include "common.h"
 
-typedef struct node
-{
-    int index;
+typedef struct node{
+    node *next;
+    node *prev;    
+    int key;
     int value;
-    struct node *next;
-    struct node *prev;
 } node;
 
-typedef struct list
-{
+typedef struct list{
     node *head;
     node *tail;
     ull size;
 } list;
 
 void create_list(list *lst);
-int list_push_back(list *lst, int index, int value);
+int list_push_back(list *lst, int key, int value);
 int list_pop_back(list *lst);
-int list_push_front(list *lst, int index, int value);
+int list_push_front(list *lst, int key, int value);
 int list_pop_front(list *lst);
 int list_delete_node(list *lst, node *n);
 void list_free(list *lst);
