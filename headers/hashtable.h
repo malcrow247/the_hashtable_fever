19a9a20adf844a0288638b843fc8a6b6bd6081cc
malcrow <melehassyti@gmail.com>
2026-03-12 19:11:53 +0300

    hashtable.h

--- headers/hashtable.h
+++ headers/hashtable.h
@@ -3,23 +3,22 @@
 
 #include "common.h"
 #include "list.h"
-#include "vector.h"
+#include "vector_list.h"
 
-#define HASHTABLE_START_SIZE 10000
-
-typedef struct hashtable
-{
-    vector data;
-    ull size;
-    ull hash_mult;
-    ull hash_plus;
+// hash(x) = k * x + b
+typedef struct hashtable {
+    vector_list data;  // вектор списков
+    ull size;  // количество элементов в хеш-таблице
+    int k;
+    int b;
 } hashtable;
 
-int hashtable_create(hashtable *h);
-void hashtable_free(hashtable *h);
-int hashtable_resize(hashtable *h, ull new_size);
-int hashtable_set_value(hashtable *h, int index, int value);
-int hashtable_get_value(hashtable *h, int index, int *result);
-int hashtable_delete(hashtable *h, int index);
+int hashtable_create(hashtable* h);
+void hashtable_free(hashtable* h);
 
-#endif // HASHTABLE_H
+int hashtable_set(hashtable* h, int key, int value);
+int hashtable_delete(hashtable* h, int key);
+
+int hashtable_search(hashtable* h, int key, int* return_value);
+
+#endif  // HASHTABLE_H
